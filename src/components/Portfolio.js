20c90017584245b4e75ddef795b1d94f96e0cdec
ThreeHDM <juanbilardi@gmail.com>
2021-08-06 12:37:44 -0300

    portfolio style changes, personal projects updated

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,17 +10,20 @@ const Portfolio = ({ language }) => {
                 {data.PORTFOLIO.map(element => (
                     <li className="cards__item">
                     <div className="card">
-                    <img src={require(`../assets/img/${element.img}`)} />
                       <div className="card__content">
                         <div className="card__title">{element[language].title}</div>
                         <p className="card__text">{element[language].description}</p>
+                        <div className="card__stack">
                         <h4>Stack:</h4>
                             {element.stack.map(el => (
-                                <p>{el}</p>
+                                <span> {el} / </span>
                             ))}
+                        </div>
+                       
                             <div className="card__links">
-                                <a href={element.code}  className="link">Code</a>
-                                <a href={element.demo}  className="link">Demo</a>
+                                <a href={element.code}  className="link" target="_blank">Code</a>
+                                { element.demo == '#' ? "" : <a href={element.demo}  className="link" target="_blank">Demo</a>}
+                                
                             </div>
                       </div>
                     </div>
